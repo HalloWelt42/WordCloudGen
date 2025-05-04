78626d424a7d8df43f409a063fd389ba748e0b02
Alpha <my@jobmagnetix.de>
2025-05-04 17:10:46 +0200

    refact

--- word_count.py
+++ word_count.py
@@ -3,6 +3,10 @@ from collections import Counter
 
 
 class WordCounter:
+    """
+    Eine Klasse, die die häufigsten Wörter in einer Textdatei zählt
+    und dabei Stoppwörter und Satzzeichen ignoriert.
+    """
 
     def __init__(self, file, stopword_file='stopwords.txt', min_word_len=2, use_stopwords=True):
         self.__file = file
@@ -25,13 +29,13 @@ class WordCounter:
         with (open(self.__file) as file):
             for line in file:
                 words = line.split()
-                # Entfernen von Satzzeichen und Umwandeln in Kleinbuchstaben
                 for word in words:
                     # Entfernen von Satzzeichen und Umwandeln in Kleinbuchstaben
                     word = re.sub(r'[^\w\s]', '', word).lower()
                     if not word or len(word) < self.__min_word_len:
                         continue
 
+                    # Stoppwörter entfernen
                     if self.__use_stopwords and word in self.__stop_words:
                         continue
 
@@ -40,7 +44,7 @@ class WordCounter:
     def most_common_words(self, list_length=0):
         self.__iterator_textfile()
 
-        # Zähle die Wörter
+        # Erstelle eine Liste der Wörter
         words = []
         for word in self.__iterator_textfile():
             words.append(word)
@@ -58,6 +62,7 @@ class WordCounter:
 
 
 def main():
+
     word_counter = WordCounter(file="beitrag.txt")
 
     # Ausgabe der häufigsten Wörter
